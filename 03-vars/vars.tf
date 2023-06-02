7047189a780faca89b68397900b86955a36affd4
IT1089778 <nvrnagella@gmail.com>
2023-06-02 14:53:46 +0530

    variable types

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,7 +1,30 @@
+#default variable type
 variable "sample" {
   default = 10
 }
 
 output "sample" {
   value = var.sample
+}
+#list variable type
+variable "sample1" {
+  default = [
+  100,"abc","xyz"
+  ]
+}
+
+output "sample1" {
+  value = var.sample1[1]
+}
+#map variable type
+variable "sample2" {
+  default = {
+    number = 100
+    string = "xyz"
+    boolean = false
+  }
+}
+
+output "sample2" {
+  value = var.sample2["number"]
 }
