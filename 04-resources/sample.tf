c911ef29cdb44e63f2e904e81ae9627bdf418c9f
IT1089778 <nvrnagella@gmail.com>
2023-06-07 11:58:31 +0530

    resource to create instance in aws

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "test" {
-
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+resource "aws_instance" "web" {
+  ami = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "test-centos8"
+  }
 }
