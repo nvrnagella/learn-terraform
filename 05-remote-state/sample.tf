923485b56bfb2b2ae9abd689ddfdd6829f3c4f14
IT1089778 <nvrnagella@gmail.com>
2023-06-07 12:38:04 +0530

    creating security group and assigning to instance

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -9,6 +9,7 @@ data "aws_ami" "centos8" {
 resource "aws_instance" "web" {
   ami = data.aws_ami.centos8.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = "test-centos8"
   }
@@ -18,5 +19,27 @@ terraform {
     bucket = "terraform-b70"
     key    = "05-remote-state/sample.tf"
     region = "us-east-1"
+  }
+}
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
   }
 }
