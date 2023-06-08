38b07a671738e17c6002edbcd8341900a056325b
IT1089778 <nvrnagella@gmail.com>
2023-06-08 23:20:17 +0530

    loops in terraform count is one of them

---
+++ 08-loops/count/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "test-centos8"
+  }
+}
+data "aws_ami" "centos8" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
