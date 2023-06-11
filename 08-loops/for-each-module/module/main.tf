a9f5b9af77dd24b1be5e201cdb64b0fb9cf0b5f3
IT1089778 <nvrnagella@gmail.com>
2023-06-11 12:00:09 +0530

    iterating in module

--- 08-loops/for-each-module/module/main.tf
+++ 08-loops/for-each-module/module/main.tf
@@ -12,32 +12,11 @@ data "aws_ami" "centos8" {
   name_regex = "Centos-8-DevOps-Practice"
 }
 
-variable "components" {
-  default = {
-    cart = {
-      name = "cart",
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      name = "catalogue",
-      instance_type = "t3.micro"
-    }
-  }
-}
+
 variable "name" {}
 variable "instance_type" {}
 
-module "ec2" {
-  source = "./module"
-  for_each = var.components
-  instance_type = each.value.instance_type
-  name = each.value.name
 
-}
-#i want to print all the output of a module after module itself
-output "publicip" {
-  value = module.ec2
-}
 #output "publicip" {
 #  value = {
 #    for k, v in aws_instance.web : k=> v.public_ip
